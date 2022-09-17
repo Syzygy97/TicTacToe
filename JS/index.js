267dde27a20cf5898b7f4cd9a81b67d256035687
Syzygy97 <aimielmarvin26@gmail.com>
2022-09-17 15:50:16 +0800

    added read board data function

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,12 @@
 const x_class = "x";
 const o_class = "o";
 const board = document.querySelector(".ticTacToe-container");
-const squares = document.querySelectorAll(".squares");
-const undoButton = document.querySelector("undo");
-const redoButton = document.querySelector("redo");
+const squares = document.querySelectorAll("[data-square]");
+const undoButton = document.querySelector("[data-undo-button]");
+const redoButton = document.querySelector("[data-redo-button]");
+const restartButton = document.querySelector("[data-restart-button]");
 const resultMessage = document.querySelector(".result");
 const winningMessage = document.querySelector("[data-winner]");
-const restartButton = document.querySelector("[data-restart-button]");
 const restartButtonInResults = document.querySelector("#restartBtnInResults");
 const winning_combinations = [
   [0, 1, 2],
@@ -19,15 +19,30 @@ const winning_combinations = [
   [6, 7, 8],
 ];
 
+const deepClone = (items) =>
+  items.map((item) => (Array.isArray(item) ? deepClone(item) : item));
+
 let board_data = [
   [0, 0, 0],
   [0, 0, 0],
   [0, 0, 0],
 ];
+
+let history_data = [];
 // let xTurn;
 let player = 1;
+let moves = 0;
 
 gameStart();
+readBoardData();
+
+undoButton.addEventListener("click", () => {
+  undoMove();
+});
+
+redoButton.addEventListener("click", () => {
+  redoMove();
+});
 
 restartButton.addEventListener("click", gameStart);
 restartButtonInResults.addEventListener("click", gameStart);
@@ -36,6 +51,7 @@ function clickEvent(e) {
   const cell = e.target;
   const currentClass = xTurn ? x_class : o_class;
   storeBoardData(cell);
+  readBoardData();
   placeMark(cell, currentClass);
   if (checkForWin(currentClass)) {
     gameEnd(false);
@@ -53,16 +69,31 @@ function storeBoardData(cell) {
   let row = (indexPos - col) / 3;
   board_data[row][col] = player;
   player *= -1;
-  // clearBoardDataOnRestart(row, col);
-  console.log(board_data);
+  console.log("board", board_data);
+  history_data[moves] = deepClone(board_data);
+  moves++;
+  console.log("history", history_data);
 }
 
-// function clearBoardDataOnRestart(row,col) {
-//   board_data[row][col] = 0;
-// }
+function readBoardData() {
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      if (board_data[row][col] === 1) {
+        squares[row * 3 + col].classList.add(x_class);
+      } else if (board_data[row][col] === -1) {
+        squares[row * 3 + col].classList.add(o_class);
+      }
+    }
+  }
+}
 
-function undoMove() {}
-function redoMove() {}
+function undoMove() {
+  console.log("undo");
+}
+
+function redoMove() {
+  console.log("redo");
+}
 
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass);
@@ -83,13 +114,17 @@ function showHoverClass() {
 }
 
 function gameStart() {
-  // clearBoardDataOnRestart();
+  console.log("restart");
   board_data = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
   ];
+  history_data = [];
+  moves = 0;
+  player = 1;
   xTurn = true;
+  readBoardData();
   squares.forEach((square) => {
     square.classList.remove(x_class);
     square.classList.remove(o_class);
